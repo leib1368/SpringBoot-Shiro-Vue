49140e422a348dce580abeb6be7b49606b29e66c
leib <leib1368@gmail.com>
2022-03-07 21:23:22 +0800

    update

---
+++ back/src/test/java/com/heeexy/example/dao/TestDao.java
@@ -0,0 +1,24 @@
+package com.heeexy.example.dao;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDao {
+
+    @Autowired
+    ArticleDao articleDao ;
+
+    @Test
+    public void A(){
+        JSONObject jsonObject = new JSONObject();
+        //articleDao.listArticle(jsonObject);
+        int i = articleDao.countArticle(jsonObject);
+        System.out.println(i);
+    }
+}
