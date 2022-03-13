a335b6847a8904c73a22de1677d09f36159eb7ea
leib <leib1368@gmail.com>
2022-03-13 14:49:17 +0800

    master分支 增加D

--- back/src/test/java/com/heeexy/example/dao/TestDao.java
+++ back/src/test/java/com/heeexy/example/dao/TestDao.java
@@ -27,6 +27,13 @@ public class TestDao {
         System.out.println("AAA");
         //after
     }
+    @Test
+    public void D(){
+        //before
+        System.out.println("AAA");
+        //after
+    }
+
 }
 
 
